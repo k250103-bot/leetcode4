e0284faace45ab2928f956497e4a0b7ecb2b6588
Shayan Ahmed <k250103@nu.edu.pk>
2025-12-07 12:24:17 +0500

    Add files via upload

---
+++ leetcode4 q1.c
@@ -0,0 +1,17 @@
+int climbStairs(int n) {
+    if (n <= 2) {
+        return n;
+    }
+
+    int a = 1;
+    int b = 2;
+    int result = 0;
+
+    for (int i = 3; i <= n; ++i) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+
+    return b;
+}
